b67bb393846c47a3264bc5d995443aca35dd4e3d
Samirka7 <90614000+Samirka7@users.noreply.github.com>
2021-12-09 23:01:15 +0700

    Update preloader.js

--- src/components/common/preloader/preloader.js
+++ src/components/common/preloader/preloader.js
@@ -1,1 +1,12 @@
+import preloader from "../../../assets/images/preloader.gif";
+import styles from "../../Users/users.module.css";
 
+let Preloader = (props) => {
+  return (
+    <div>
+      <img className={styles.preloader} src={preloader} />
+    </div>
+  );
+};
+
+export default Preloader;
