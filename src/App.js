5f659a2d6d354b608edbec3381b14bfaf3c9efdd
Samirka7 <90614000+Samirka7@users.noreply.github.com>
2021-12-09 22:33:21 +0700

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, withRouter } from "react-router-dom";
+import UsersContainer from "./components/Users/UsersContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/Login/Login";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { initializeApp } from "./redux/app-reducer";
+import Preloader from "./components/common/preloader/preloader";
+import { withSuspense } from "./hoc/withSuspence";
+const DialogsContainer = React.lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
+const ProfileContainer = React.lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.initializeApp();
+  }
+
+  render() {
+    if (!this.props.initialized) {
+      return <Preloader />;
+    }
+
+    return (
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+
+          <Route
+            path="/profile/:userId?"
+            render={withSuspense(ProfileContainer)}
+          />
+          <Route path="/users" render={() => <UsersContainer />} />
+
+          <Route path="/login" render={() => <LoginPage />} />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized,
+});
+
+export default compose(
+  withRouter,
+  connect(mapStateToProps, { initializeApp })
+)(App);
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,78 @@
+import { useFormik } from "formik";
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { login } from "../../redux/auth-reducer";
+
+const Login = (props) => {
+  const initialValues = {
+    email: "",
+    password: "",
+    rememberMe: false,
+  };
+
+  const onSubmit = (values) => {
+    props.login(
+      formik.values.email,
+      formik.values.password,
+      formik.values.rememberMe
+    );
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  if (props.isAuth) {
+    return <Redirect to={"/profile"} />;
+  }
+
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <input
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <label>login</label>
+          {props.errorCod !== 0 ? <div>{props.errorName}</div> : null}
+        </div>
+        <div>
+          <input
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <label>password</label>
+        </div>
+        <div>
+          <input
+            value={formik.values.rememberMe}
+            onChange={formik.handleChange}
+            name="rememberMe"
+            type="checkbox"
+          />
+          remember me
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  isAuth: state.auth.isAuth,
+  errorCod: state.auth.errorCod,
+  errorName: state.auth.errorName,
+});
+export default connect(mapStateToProps, { login })(Login);
